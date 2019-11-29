145b6e07c11ca72b4eb26788abf82e6969b79114
Exebixel <ezequielnat7@gmail.com>
2019-11-29 19:15:35 -0300

    lê os arquivos no diretorio e cria um arquivo segundo a númereração correta

--- file.c
+++ file.c
@@ -1,16 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <dirent.h>
 
 void file()
 {
+    DIR *diretorio = opendir("History");
+
+    int cont = 0;
+    while (readdir(diretorio))
+    {
+        cont++;
+    }
+    printf("Número de arquivos: %d \n", cont-2);
+    closedir(diretorio);
+    
     FILE *arquivo;
     char nome[40];
-    int num = 3;
-    sprintf(nome, "History/provas-%i", num);
-    printf("%s", nome);
+    sprintf(nome, "History/provas-%i", cont-1);
     arquivo = fopen(nome, "w");
+    printf("Nome do arquivo criado: %s \n", nome);
     printf("Arquivo criado com sucesso!!! \n");
     fclose(arquivo);
+    
 }
 
 int main()
