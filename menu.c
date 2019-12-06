f7c0017614c6514843c7cda8e472a468618386ce
Exebixel <ezequielnat7@gmail.com>
2019-12-06 17:55:56 -0300

    Add function for centralize window and text

--- menu.c
+++ menu.c
@@ -2,14 +2,18 @@
 #include <stdlib.h>
 #include <locale.h>
 #include <ncurses.h>
+#include <string.h>
 
+void menu();
+WINDOW *createCentralizeWindow(int lines, int columns);
+void printwcentralize(WINDOW *window, int line, int totalColumns, char *text);
 
 void menu()
 {
     setlocale(LC_ALL, "");
     // inicia o ncurses
     initscr();
-
+    curs_set(0);
     // inicia suporte a cores
     start_color();
     // cria um par de cores cor do foreground branco e
@@ -22,35 +26,15 @@ void menu()
     // atualiza interface
     refresh();
 
-    // variaveis com tamanho da janela 
-    int windowLines = 20;
-    int windowColumns = 50;
-
-    int windowSizeColum = COLS;
-    int windowSizeLine = LINES; 
-
-    // if para limitar tamanho maxino da janela ao tamanho do terminal
-    if (windowLines > windowSizeLine)
-    {
-        windowLines = windowSizeLine;
-    }
-    if (windowColumns > windowSizeColum)
-    {
-        windowColumns = windowSizeColum;
-    }    
-
-    // calculo para centralizar janela 
-    windowSizeColum/=2;
-    windowSizeLine/=2;
-    windowSizeColum -= windowColumns/2;
-    windowSizeLine -= windowLines/2;
-
-    // criação da janela
+    // cria uma janela com 30 linhas e 60 colunas 
+    // e centraliza ela na tela
     WINDOW *window;
-    window = newwin(windowLines, // númeoro de linhas da janela
-                    windowColumns, // número de colunas 
-                    windowSizeLine, // linha que inicia a janela
-                    windowSizeColum); // coluna que inicia a janela
+    window = createCentralizeWindow(30, 60);
+
+    printwcentralize(window, 1, 60, "MENU \n");
+    //wprintw(window, "Corrigir prova");
+    printwcentralize(window, 2, 60, "Corrigir prova");
+
     // atualiza janela
     wrefresh(window);
     
@@ -61,6 +45,47 @@ void menu()
     delwin(window);
     // fecha ncurses
     endwin();
+
+}
+
+WINDOW *createCentralizeWindow(int lines, int columns)
+{
+    int windowSizeColum = COLS;
+    int windowSizeLine = LINES; 
+
+    // if para limitar tamanho maxino da janela ao tamanho do terminal
+    if (lines > windowSizeLine)
+    {
+        lines = windowSizeLine;
+    }
+    if (columns > windowSizeColum)
+    {
+        columns = windowSizeColum;
+    }    
+
+    // calculo para centralizar janela 
+    windowSizeColum/=2;
+    windowSizeLine/=2;
+    windowSizeColum -= columns/2;
+    windowSizeLine -= lines/2;
+
+    // criação da janela
+    WINDOW *window;
+    window = newwin(lines, // númeoro de linhas da janela
+                    columns, // número de colunas 
+                    windowSizeLine, // linha que inicia a janela
+                    windowSizeColum); // coluna que inicia a janela
+
+    return window;
+}
+
+void printwcentralize(WINDOW *window, int line, int totalColumns, char *text)
+{
+    int column;
+    int sizeText = strlen(text);
+    column = (totalColumns/2)-(sizeText/2);
+    
+    mvwprintw(window, line, column, text);
 
 }
 
