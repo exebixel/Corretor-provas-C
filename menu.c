f6ce15949f0543d7c2defcfa213b489a537cf783
Exebixel <ezequielnat7@gmail.com>
2019-12-07 07:45:16 -0300

    Print menu and message box

--- menu.c
+++ menu.c
@@ -4,16 +4,23 @@
 #include <ncurses.h>
 #include <string.h>
 
+#define clearBuffer while(getchar() != '\n');
+#define ENTER 10
+
 void menu();
+// cria uma janela centralizada na tela do terminal
 WINDOW *createCentralizeWindow(int lines, int columns);
+// imprime um texto centralizado na tela
 void printwcentralize(WINDOW *window, int line, int totalColumns, char *text);
+int messageBox(char *menssage);
 
 void menu()
 {
-    setlocale(LC_ALL, "");
     // inicia o ncurses
     initscr();
     curs_set(0);
+    //abilita o uso dos #defines do teclado, possibilita, por exemplo a referência da tecla F1 por KEY_F1
+    keypad(stdscr, TRUE); 
     // inicia suporte a cores
     start_color();
     // cria um par de cores cor do foreground branco e
@@ -29,11 +36,23 @@ void menu()
     // cria uma janela com 30 linhas e 60 colunas 
     // e centraliza ela na tela
     WINDOW *window;
-    window = createCentralizeWindow(30, 60);
+    window = createCentralizeWindow(15, 50);
+    // cria uma borda na tela
+    box(window, ACS_VLINE, ACS_HLINE);
 
-    printwcentralize(window, 1, 60, "MENU \n");
-    //wprintw(window, "Corrigir prova");
-    printwcentralize(window, 2, 60, "Corrigir prova");
+    printwcentralize(window, 1, 50, "MENU PRINCIPAL");
+
+    char *text[6];
+    text[0] = "1 - Cadastro de aluno";
+    text[1] = "2 - Editar turmas";
+    text[2] = "3 - Correção de prova";
+    text[3] = "4 - Historico de provas";
+    text[4] = "5 - Log out";
+    text[5] = "6 - Fechar programa";
+    for (int i = 0; i < 6; i++)
+    {
+        mvwprintw(window, (i+3), 3, text[i]);
+    }
 
     // atualiza janela
     wrefresh(window);
@@ -41,10 +60,16 @@ void menu()
     // aguarda usuario digita alguma tecla
     getch();
 
+   
+    messageBox("Em Desenvolvimento");
+   
+    //getch();
+
     // deleta a janela
     delwin(window);
     // fecha ncurses
     endwin();
+    exit(0);
 
 }
 
@@ -82,15 +107,38 @@ WINDOW *createCentralizeWindow(int lines, int columns)
 void printwcentralize(WINDOW *window, int line, int totalColumns, char *text)
 {
     int column;
+    // pega o tamanho do texto a ser "imprimido"
     int sizeText = strlen(text);
+    // calcula a coluna inicial do texto (onde ele começa)
     column = (totalColumns/2)-(sizeText/2);
-    
+    // imprime na tela
     mvwprintw(window, line, column, text);
+}
 
+int messageBox(char *message)
+{
+    WINDOW *winbox;
+    // cria uma janela centralizada de 5/15
+    winbox = createCentralizeWindow(5, 30);
+    // cria uma linha al redor da janela
+    box(winbox, ACS_VLINE, ACS_HLINE);
+    printwcentralize(winbox, 1, 30, message);
+    wrefresh(winbox);
+    //clearBuffer;
+    int key;
+    do
+    {
+        key = getch();
+    } while (key != ENTER);
+    
+    delwin(winbox);
+    
+    
 }
 
 int main()
 {
+    setlocale(LC_ALL, "");
     menu();
     return 0;
 }
