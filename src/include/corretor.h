d91dfe787c01058e83375a71946130527123e466
exebixel <ezequielnat7@gmail.com>
2020-01-13 20:21:28 -0300

    Comentarios no header do corretor adicionados

--- src/include/corretor.h
+++ src/include/corretor.h
@@ -1,9 +1,17 @@
 #ifndef CORRETOR_H
 #define CORRETOR_H
 
+// guarda o número de questões cadastradas pelo usuario
 int qtdQuestoes;
+
+// guarda o número de alunos cadastrados
 int qtdAlunos;
+
+// vetor com o gabarito 
+// com as respostas certas
 char gabarito[100];
+
+// guarda todos os nomes dos alunos cadastrados
 char nomeAlunos[100][50];
 // o 1° 100 é referente as respostas do gabarito
 // o 2° 100 é referente aos alunos 
